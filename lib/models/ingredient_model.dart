16b42af88355c51d57822fc6ce41dfe278360cc4
Luis Ribeiro (CTW) <luis.ribeiro@ctw.bmwgroup.com>
2025-05-19 12:24:53 +0100

    Refactor: Update Recipe model with Ingredients and Instructions
    This commit introduces `Ingredient` and `Instruction` models and integrates them into the `Recipe` model.
    
    The `Recipe` model now includes lists of `Ingredient` and `Instruction` objects, allowing for more detailed recipe information.
    
    Mock recipe data has been updated to reflect these changes, providing examples of recipes with ingredients and instructions.
    
    The `new.dart` file, which previously contained a duplicate `Instruction` model, has been removed as the model is now defined in `instruction_model.dart`.

---
+++ lib/models/ingredient_model.dart
@@ -0,0 +1,13 @@
+// lib/models/ingredient_model.dart
+
+class Ingredient {
+  final int order;
+  final String name;
+  final String quantity;
+
+  Ingredient({
+    required this.order,
+    required this.name,
+    required this.quantity,
+  });
+}
